9245667339ffeeea9206543ca5eb69c09cf0b9c7
khonnie <mcostance@gmail.com>
2026-04-12 02:55:08 +0300

    added checks for username, email if they exist, also if the paswword match, also if the username length is greater than 10 characters and alpha numeric, and finally started implemrnting the email functionality

--- user_auth/views.py
+++ user_auth/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from user_system import settings
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -39,6 +41,29 @@ def signup(request):
         email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
+
+        
+         # we will also check if the username is already taken before registering the user, 
+        #if username already exixs then u cant create an account with that username, you have to choose another username, we will show a 
+        #message that the username is already taken and then we will redirect the user to the signup page again
+        if User.objects.filter(username=username):
+            messages.error(request, 'Username already exists, please try some other username')
+            return redirect('user_auth:home')
+        if User.objects.filter(email=email):
+            messages.error(request, 'Email already registered, please some other email')
+            return redirect('user_auth:home')
+        if len(username)>10:
+            messages.error(request, 'Username must be less than 10 characters')
+            return redirect('user_auth:home')
+
+        if password != confirm_password :
+            messages.error(request, 'Passwords didnt march!')
+            return redirect('user_auth:home')
+
+        if not username.isalnum():
+            messages.error(request, 'username must be alpha numeric')
+            return redirect()
+
        # after the user submitting their data/collecting user data, we shall register the user in the database and save the user data in the database
        # we will make use of a django inbuilt user model to register the user in the database and save the user data in the database
         #to do that we first import the user model from django.contrib.auth.models import User
@@ -50,11 +75,19 @@ def signup(request):
         my_user.save() # this will save the user data in the database
         #if the user is regidtered successfully, we will show him a message that he is registered successfully and then we will redirect him to the signin page
         #for thus we are making use of messages from django by using from django.contrib import messages
-        messages.success(request, 'Your account has been successfully created.')
+        messages.success(request, 'Your account has been successfully created. we have send you an email, please confirm your email in order to activate your account')
         #as soon as the user is registered successfully, we will redirect him to the signin page
+        
+        #welcome Email, code to send an email
+        subject = 'Welcome to Experteens'
+        message = 'Hello'+ my_user.first_name + "!! \n"+ "welcome to experteens!! \n Thank you for signing up \n we have sent you a confirmation email \n please confirm your email to activate your account"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [my_user.email] #list of emailaddreses that we are sending the email
+        send_mail(subject, message, from_email, to_list, fail_silently = True)
+        
         return redirect('user_auth:signin')
         
-        # we will also check if the username is already taken before registering the user
+       
         
        # we will also check if the password and confirm password are the same before registering the user
 
---
+++ user_system/info.py
@@ -0,0 +1,7 @@
+#this file contains information about the email account
+# we will use this information to send emails to the users for password reset and account activation
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'conniebulafu@gmail.com'
+EMAIL_HOST_PASSWORD = 'Jesus100%control'
+EMAIL_PORT = 587
